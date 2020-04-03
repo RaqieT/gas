2961692eaf32f26a4405061da497b19dacf51913
Michał <sokoler16@gmail.com>
2020-04-03 14:39:32 +0200

    add colors :D

--- src/main/java/com/project/team/gas/configuration/SecurityConfig.java
+++ src/main/java/com/project/team/gas/configuration/SecurityConfig.java
@@ -7,11 +7,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity security) throws Exception
     {
         security.httpBasic().disable();
     }
+
 }
