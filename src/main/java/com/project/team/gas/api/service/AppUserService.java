e904134ba7f0b440b23d55bf3cc659a529d0e448
raqiet <dawidm@int.pl>
2020-04-08 20:43:03 +0200

    UI

--- src/main/java/com/project/team/gas/api/service/AppUserService.java
+++ src/main/java/com/project/team/gas/api/service/AppUserService.java
@@ -1,4 +1,7 @@
 package com.project.team.gas.api.service;
 
+import com.project.team.gas.datastore.AppUser;
+
 public interface AppUserService {
+    AppUser getMe();
 }
--- src/main/java/com/project/team/gas/controller/WebAppController.java
+++ src/main/java/com/project/team/gas/controller/WebAppController.java
@@ -1,5 +1,11 @@
 package com.project.team.gas.controller;
 
+import com.project.team.gas.api.service.AppUserService;
+import com.project.team.gas.datastore.AppUser;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Date;
 
 @Controller
+@RequiredArgsConstructor
 public class WebAppController {
+    private final AppUserService appUserService;
+    private final Logger mLog = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("datetime", new Date());
-        model.addAttribute("username", " Gas");
+        var me = appUserService.getMe();
 
+        model.addAttribute("username", me.getName());
+        model.addAttribute("user_img", me.getImageUrl());
         return "index";
     }
 
